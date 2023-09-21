3142296357dedfce46ddae441c4a08c2778e7c20
Anderson Silva <anderson.consultant.it@outlook.com>
2023-09-20 22:45:18 -0300

    feat: footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,8 +1,17 @@
+import Link from 'next/link'
 import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className='fixed bottom-0 flex-between body-text w-full gap-y-10 border-t 
+    border-black-400 text-white-800 bg-black-100 px-20 py-12 max-md:flex-col'>
+      <p>Copyright © 2023 JS Mastery Pro | All Rights Reserved</p>
+      
+      <div className='flex gap-x-9'>
+        <Link href='/terms-of-use'>Terms & Conditions</Link>
+        <Link href='/privacy-policy'>Privacy Policy</Link>
+      </div>
+    </footer>
   )
 }
 
