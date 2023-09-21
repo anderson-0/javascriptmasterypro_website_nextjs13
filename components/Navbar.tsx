88256f8599ae5bf3af6bd88be8f17f7df5f15ad0
Anderson Silva <anderson.consultant.it@outlook.com>
2023-09-20 22:18:45 -0300

    feat: navbar initial styling

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,13 @@
+import Link from 'next/link'
 import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className='flex-center fixed top-0 z-50 w-full border-b-2 border-black-200 py-7 text-white'>
+      <div className='flex-between mx-auto w-full max-w-screen-2xl px-6 xs:px-8 sm:px-16'>
+        <Link href="/">Home</Link>
+      </div>
+    </nav>
   )
 }
 
