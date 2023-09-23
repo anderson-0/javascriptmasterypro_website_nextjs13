bb8fc8b37f9202657bc6d950655e4061db0b45cf
Anderson Silva <anderson.consultant.it@outlook.com>
2023-09-23 13:41:28 -0300

    feat: sanity configuration

---
+++ sanity/actions.ts
@@ -0,0 +1,34 @@
+import { groq } from 'next-sanity';
+import { readClient } from './lib/client';
+import { buildQuery } from './utils';
+
+interface GetResourcesParams {
+  query: string;
+  category: string;
+  page: string;
+}
+
+export const getResources = async (params: GetResourcesParams) => {
+  const { page, query, category } = params;
+  
+  try {
+    const resources = await readClient.fetch(groq`${buildQuery({
+      type: 'resource',
+      query,
+      category,
+      page: parseInt(page),
+    })} {
+      _id,
+      title,
+      downloadLink,
+      "image": poster.asset->url,
+      views,
+      slug,
+      category
+    }`)
+    return resources;
+  } catch (error) {
+    console.log(error);
+  }
+
+}
