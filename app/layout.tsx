f9a0f738c8a0d010a8d2300dcc7b83a9728482b7
Anderson Silva <anderson.consultant.it@outlook.com>
2023-09-24 13:59:19 -0300

    fix: supress hydration warnings

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className='min-h-screen bg-black-100'>{children}</body>
+      <body className='min-h-screen bg-black-100' suppressHydrationWarning={true}>
+        {children}
+      </body>
     </html>
   )
 }
