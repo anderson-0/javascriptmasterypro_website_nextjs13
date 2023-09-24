4f0f9d4f21f09a209c3150a82f1d49cd156e39f9
Anderson Silva <anderson.consultant.it@outlook.com>
2023-09-24 15:54:46 -0300

    fix: fixed flex-col and wrong closed section and added resourcePlaylist headers

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -2,7 +2,7 @@ import Filters from '@/components/Filters'
 import Header from '@/components/Header'
 import ResourceCard from '@/components/ResourceCard'
 import SearchForm from '@/components/SearchForm'
-import { getResources } from '@/sanity/actions'
+import { getResources, getResourcePlaylist } from '@/sanity/actions'
 import React from 'react'
 
 export const revalidate = 900; // 15 minutes
@@ -17,44 +17,51 @@ const Home = async ({ searchParams }: HomeProps) => {
     category: searchParams?.category || '',
     page: '1'
   });
+
+  const resourcePlaylist = await getResourcePlaylist();
+
   return (
-    <main className='flex-center paddings mx-auto w-full max-w-screen-2xl flex-column'>
+    <main className='flex-center paddings mx-auto w-full max-w-screen-2xl flex-col'>
       <section className='nav-padding w-full'>
         <div className='flex-center relative min-h-[274px] w-full flex-col rounded-xl bg-banner bg-cover bg-center text-center'>
-          <h1 className='sm:heading1 heading2 mb-6 text-white'>Javascript Mastery Resources</h1>
+          <h1 className='sm:heading1 heading2 mb-6 text-white text-center'>Javascript Mastery Resources</h1>
         </div>
         <SearchForm/>
-        <Filters/>
-        {(searchParams?.query || searchParams?.category) && (
-          <section className='flex-center mt-6 w-full flex-col sm:mt-20'>
-            <Header
-              type='Resources'
-              query={searchParams?.query || ''}
-              category={searchParams?.category || ''}
-            />
-            <div className='mt-12 flex w-full flex-wrap justify-center
-          gap-16'>
-              {resources?.length > 0 ? (
-                resources.map((resource: any) => (
-                  <ResourceCard
-                    key={resource._id}
-                    title={resource.title}
-                    id={resource._id}
-                    image={resource.image}
-                    downloadNumber={resource.views}
-                    downloadLink={resource.downloadLink}
-                  />
-                ))
-              ) : (
-                <p className='body-regular text-white-400'>
-                  No resources found
-                </p>
-              )}
-            </div>
-          </section>
-        )}
-        
       </section>
+      <Filters/>
+      {(searchParams?.query || searchParams?.category) && (
+        <section className='flex-center mt-6 w-full flex-col sm:mt-20'>
+          <Header
+            type='Resources'
+            query={searchParams?.query || ''}
+            category={searchParams?.category || ''}
+          />
+          <div className='mt-12 flex w-full flex-wrap justify-center
+        gap-16'>
+            {resources?.length > 0 ? (
+              resources.map((resource: any) => (
+                <ResourceCard
+                  key={resource._id}
+                  title={resource.title}
+                  id={resource._id}
+                  image={resource.image}
+                  downloadNumber={resource.views}
+                  downloadLink={resource.downloadLink}
+                />
+              ))
+            ) : (
+              <p className='body-regular text-white-400'>
+                No resources found
+              </p>
+            )}
+          </div>
+        </section>
+      )}
+      {resourcePlaylist.map((item: any) => (
+        <section key={item._id} className='flex-center mt-6 w-full flex-col sm:sm:mt-20'>
+          <h1 className='heading3 self-start text-white-800'>{item.title}</h1>
+        </section>
+      ))}
     </main>
   )
 }
